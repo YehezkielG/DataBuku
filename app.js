ea2727a716db69df07753c9cb5a351359a6e6078
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-07-31 13:03:55 +0700

    upadate mjs to js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from "express";
-import expressEjsLayouts from "express-ejs-layouts";
-import bodyParser from "body-parser";
-import morgan from "morgan";
+const express =  require("express");
+const expressEjsLayouts =  require("express-ejs-layouts");
+const bodyParser =  require("body-parser");
+const morgan =  require("morgan");
 
 const app = express();
 const port = 3000;
@@ -30,4 +30,4 @@ app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
 })
 
-export default app
+module.exports = app
